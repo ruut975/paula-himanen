71dca25959a5de38674e8cb1a564ca7bef7ca3d9
Ruut <ruut.sofia@gmail.com>
2021-03-06 18:14:50 +0200

    footer link hover style added

--- components/SocialIcons/SocialIcons.js
+++ components/SocialIcons/SocialIcons.js
@@ -5,9 +5,9 @@ import styles from './SocialIcons.module.css'
 
 const SocialIcons = () => (
       <div className={styles.socialFlex}>
-         <a href="https://www.facebook.com/himanenpaulavihreat/" rel="noopener noreferrer"><Image src="/facebook-icon.svg" alt="facebook" width={32} height={32} /></a>
-         <a href="https://twitter.com/himanenpaula?lang=en" rel="noopener noreferrer"><Image src="/twitter-icon.svg" alt="twitter" width={32} height={32} /></a>
-         <a href="#"><Image src="/instagram-icon.svg" alt="instagram" width={32} height={32} /></a>
+         <a href="https://www.facebook.com/himanenpaulavihreat/" rel="noopener noreferrer"><Image src="/facebook-icon.svg" alt="facebook" width={35} height={35} /></a>
+         <a href="https://twitter.com/himanenpaula?lang=en" rel="noopener noreferrer"><Image src="/twitter-icon.svg" alt="twitter" width={35} height={35} /></a>
+         <a href="#"><Image src="/instagram-icon.svg" alt="instagram" width={35} height={35} /></a>
       </div>
 )
  
