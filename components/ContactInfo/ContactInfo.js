d0898e94347995d505474277ddd719b07d000b10
Ruut <ruut.sofia@gmail.com>
2021-03-06 17:55:26 +0200

    p margins resetted

--- components/ContactInfo/ContactInfo.js
+++ components/ContactInfo/ContactInfo.js
@@ -4,9 +4,9 @@ import styles from './ContactInfo.module.css'
 const ContactInfo = () => (
    <div className={styles.flexContainer}>
      <h3 className={styles.header}>Ota Yhteyttä</h3>
-     <p className={styles.text}>Paula Himanen</p>
-     <a href="mailto:paula.himanen@vihreat.fi" target="_blank" rel="noopener noreferrer" className={styles.text}>paula.himanen@vihreat.fi</a>
-     <a href="tel:+358-405059634" rel="nofollow" className={styles.text}>puh: 0405059634</a>
+     <p>Paula Himanen</p>
+     <a href="mailto:paula.himanen@vihreat.fi" target="_blank" rel="noopener noreferrer">paula.himanen@vihreat.fi</a>
+     <a href="tel:+358-405059634" rel="nofollow">puh: 0405059634</a>
    </div>
 )
  
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import 'normalize.css'
+import 'sanitize.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
