f18f71bbe95c823ed0aeb28907a0ea0b0eb8c5b3
Ruut <ruut.sofia@gmail.com>
2021-03-11 12:41:22 +0200

    padding modified

--- components/BlogPostCards/BlogPostCards.js
+++ components/BlogPostCards/BlogPostCards.js
@@ -1,16 +1,18 @@
 import PostCard from "./BlogPostCard/BlogPostCard";
-import ReadMoreLink from '../../UI/ReadMoreLink/ReadMoreLink'
+import ReadMoreLink from "../../UI/ReadMoreLink/ReadMoreLink";
 import styles from "./BlogPostCards.module.css";
 
 const BlogPostCards = ({ posts }) => {
   const fourNewestPosts = posts.slice(0, 4);
-  console.log(fourNewestPosts)
+  console.log(fourNewestPosts);
 
   return (
     <section className={styles.blogPostCards}>
       <h1 className={styles.title}>Blogi</h1>
-      <ReadMoreLink hoverGreen bold path="/blogi">Kaikki Postaukset</ReadMoreLink>
-      {fourNewestPosts.map(post => (
+      <ReadMoreLink hoverGreen bold path="/blogi">
+        Kaikki Postaukset
+      </ReadMoreLink>
+      {fourNewestPosts.map((post) => (
         <PostCard key={post.id} post={post} />
       ))}
     </section>
